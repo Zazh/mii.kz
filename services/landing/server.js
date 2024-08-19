af7e956899cb2ee0656e8fa678e73982bfa906da
Aidarbek <aidarbek@MacBook-Pro-Aidarbek.local>
2024-08-19 21:52:23 +0500

    add Homapge service

--- services/landing/server.js
+++ services/landing/server.js
@@ -1,19 +1,16 @@
-// services/landing/server.js
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-// Настройка статической папки для обслуживания файлов
-app.use(express.static(path.join(__dirname, 'public')));
+// Обслуживание статических файлов
+app.use(express.static('public'));
 
-// Обработка корневого маршрута
+// Корневой маршрут
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 
-// Запуск сервера на порту 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Landing сервис запущен на порту ${PORT}`);
+    console.log(`Landing page сервис запущен на порту ${PORT}`);
 });
